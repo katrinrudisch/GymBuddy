78e68031071b129775a2345f2712e446ec444df3
Katrin <katrin.rudisch@gmail.com>
2022-03-12 23:48:05 +0100

    Remove fragment

--- app/src/main/java/com/katrinrudisch/gymbuddy/ui/MainActivity.kt
+++ app/src/main/java/com/katrinrudisch/gymbuddy/ui/MainActivity.kt
@@ -1,13 +1,49 @@
 package com.katrinrudisch.gymbuddy.ui
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.katrinrudisch.gymbuddy.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import com.katrinrudisch.gymbuddy.compose.GymBuddyTheme
+import com.katrinrudisch.gymbuddy.models.Plan
+import com.katrinrudisch.gymbuddy.ui.components.Cell
+import com.katrinrudisch.gymbuddy.ui.components.StatefulLayout
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel by viewModel<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            GymBuddyTheme {
+                PlansList()
+            }
+        }
+    }
+
+    @Composable
+    fun PlansList() {
+        val state = viewModel.planState.value
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            StatefulLayout(state) { data ->
+                LazyColumn {
+                    data.forEach {
+                        item {
+                            Cell(it.title, "")
+                        }
+                    }
+                }
+            }
+
+            FloatingActionButton(onClick = { viewModel.addPlan(Plan(title = "Some plan")) }) {
+
+            }
+        }
     }
 }
